bd562232712e796b52fa837da6a904c0a7404e90
Sommerabend <leonr2000@gmx.de>
2026-09-17 19:06:41 +0200

    Add Sunny Launcher secure preload bridge

---
+++ preload.js
@@ -0,0 +1,15 @@
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('sunnyDesktop', {
+  getPaths: () => ipcRenderer.invoke('get-paths'),
+  chooseInstallFolder: () => ipcRenderer.invoke('choose-install-folder'),
+  installGame: game => ipcRenderer.invoke('install-game', game),
+  launchInstalledGame: game => ipcRenderer.invoke('launch-installed-game', game),
+  openFolder: folder => ipcRenderer.invoke('open-folder', folder),
+  openExternal: url => ipcRenderer.invoke('open-external', url),
+  createDesktopShortcut: target => ipcRenderer.invoke('create-desktop-shortcut', target),
+  checkUpdate: () => ipcRenderer.invoke('check-update'),
+  onDownloadProgress: fn => ipcRenderer.on('download-progress', (_, data) => fn(data)),
+  onDownloadStart: fn => ipcRenderer.on('download-start', (_, data) => fn(data)),
+  onDownloadComplete: fn => ipcRenderer.on('download-complete', (_, data) => fn(data)),
+  onDownloadError: fn => ipcRenderer.on('download-error', (_, data) => fn(data))
+});
